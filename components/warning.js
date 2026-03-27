9922ec9e5ee66c371ef81f139743694a0be63816
Allan <allanhu100@gmail.com>
2026-03-27 15:50:35 -0500

    Create warning.js

---
+++ components/warning.js
@@ -0,0 +1,20 @@
+fetch('/components/warning.html')
+  .then(res => res.text())
+  .then(html => {
+    document.body.insertAdjacentHTML('afterbegin', html);
+
+    // lock page
+    document.body.classList.add('locked');
+
+    // continue
+    document.querySelector('.continue').onclick = () => {
+      document.getElementById('overlay').remove();
+      document.body.classList.remove('locked');
+    };
+
+    // return
+    document.getElementById('returnToHome').onclick = () => {
+      window.location.href = 'https://allanhu100.github.io/home?redirect=testing_playground';
+    };
+  })
+  .catch(err => console.error('Warning component failed:', err));
